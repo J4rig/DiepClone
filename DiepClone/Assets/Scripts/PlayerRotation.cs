a93ef6ec7fd53481cf84b5adf7d1238196fffd3c
J4rig <kubino434@gmail.com>
2022-12-13 21:55:01 +0100

    rotacia

---
+++ DiepClone/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [SerializeField] private Camera mainCam;
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0,0,rotZ);
+    }
+}
